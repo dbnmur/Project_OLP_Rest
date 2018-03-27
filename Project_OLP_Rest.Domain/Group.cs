12868b34015a2e8ea9e53b7012f62a6b9e38de79
Daniel <zeimodaniel@gmail.com>
2018-03-27 11:58:13 +0300

    entities

---
+++ Project_OLP_Rest.Domain/Group.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplateCore.Domain
+{
+    public class Group
+    {
+        public int GroupId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Course> Courses { get; set; }
+    }
+}
