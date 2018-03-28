69347cd6171f50cf1a5cdd2f6a7e1f5ac326757c
Lukas <MiniBig@users.noreply.github.com>
2018-03-28 11:21:09 +0300

    add teacher course tests, update teacher services

---
+++ Project_OLP_Rest.Data/Interfaces/ITeacherService.cs
@@ -0,0 +1,13 @@
+﻿using Project_OLP_Rest.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Project_OLP_Rest.Data.Interfaces
+{
+    public interface ITeacherService : IGenericService<Teacher>
+    {
+            
+    }
+}
