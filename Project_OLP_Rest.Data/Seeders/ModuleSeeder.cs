63733eae8c52fa8f80d82f4b6af4f3073a4fa07a
Airidas Xeriaz <airidasams@gmail.com>
2018-03-29 12:19:23 +0300

    Add seeders

---
+++ Project_OLP_Rest.Data/Seeders/ModuleSeeder.cs
@@ -0,0 +1,29 @@
+﻿using Project_OLP_Rest.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_OLP_Rest.Data.Seeders
+{
+    class ModuleSeeder : Seeder<Module>
+    {
+        public ModuleSeeder(OLP_Context context) : base(context) { }
+
+        public override void Run()
+        {
+            _entities.Add(new Module()
+            {
+                Name = "Module title",
+                Description = "Module description",
+            });
+
+            _entities.Add(new Module()
+            {
+                Name = "Module title 2",
+                Description = "Module description 2",
+            });
+
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Project_OLP_Rest.Data/Seeders/RecordSeeder.cs
@@ -0,0 +1,31 @@
+﻿using Project_OLP_Rest.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_OLP_Rest.Data.Seeders
+{
+    class RecordSeeder : Seeder<Record>
+    {
+        public RecordSeeder(OLP_Context context) : base(context) { }
+
+        public override void Run()
+        {
+            _entities.Add(new Record()
+            {
+                Name = "Record name",
+                Description = "Record description",
+                Url = "Record url",
+            });
+
+            _entities.Add(new Record()
+            {
+                Name = "Record name 2",
+                Description = "Record description 2",
+                Url = "Record url 2",
+            });
+
+            _context.SaveChanges();
+        }
+    }
+}
