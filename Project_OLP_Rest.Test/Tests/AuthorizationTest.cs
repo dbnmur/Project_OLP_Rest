565cddc80cd8fa172cdba2a7db68a5a86442309d
dbnmur <nerijus.muranovas@gmail.com>
2018-03-29 13:33:04 +0300

    Add AuthorizationTest

--- Project_OLP_Rest.Test/Tests/AuthorizationTest.cs
+++ Project_OLP_Rest.Test/Tests/AuthorizationTest.cs
@@ -1,16 +1,39 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 
 namespace Project_OLP_Rest.Test.Tests
 {
     [TestClass]
     public class AuthorizationTest
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+        //private readonly TestServer _server;
+        //private readonly HttpClient _client;
+        //private IConfiguration _configuration;
+        //public AuthorizationTest()
+        //{
+        //    _configuration = new ConfigurationBuilder()
+        //    .SetBasePath(Path.GetFullPath(@"../../.."))
+        //    .AddJsonFile("appsettings.json", optional: false)
+        //    .Build();
+
+        //    _server = new TestServer(new WebHostBuilder()
+        //        .UseStartup<Startup>()
+        //        .UseConfiguration(_configuration));
+        //    _client = _server.CreateClient();
+        //}
+
+        //[Fact]
+        //public async Task UnAuthorizedAccess()
+        //{
+        //    var response = await _client.GetAsync("/api/books");
+
+        //    Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        //}
     }
 }
