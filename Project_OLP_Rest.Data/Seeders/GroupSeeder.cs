6e89abceb3069b6a181ff7a95cf1d5771213216c
dbnmur <nerijus.muranovas@gmail.com>
2018-03-29 11:07:07 +0300

    Added seeder starting kit

---
+++ Project_OLP_Rest.Data/Seeders/GroupSeeder.cs
@@ -0,0 +1,29 @@
+﻿using Project_OLP_Rest.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_OLP_Rest.Data.Seeders
+{
+    public class GroupSeeder : Seeder<Group>
+    {
+        public GroupSeeder(OLP_Context context) : base(context) { }
+
+        public override void Run()
+        {
+            _entities.Add(new Group()
+            {
+                Name = "Some name",
+                Description = "Some description"
+            });
+
+            _entities.Add(new Group()
+            {
+                Name = "Some other name",
+                Description = "Some other description"
+            });
+
+            _context.SaveChanges();
+        }
+    }
+}
