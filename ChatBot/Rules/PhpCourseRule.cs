bda043e4a877bcba6eab1bbba43ac5e1f51bd79d
Airidas Xeriaz <airidasams@gmail.com>
2018-04-09 12:47:24 +0300

    Add PhpCourseRule.cs and PHPCourseRegexTest

--- ChatBot/Rules/PhpCourseRule.cs
+++ ChatBot/Rules/PhpCourseRule.cs
@@ -2,11 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace QXS.ChatBot.Rules
 {
-    class PhpCourseRule
+    public class PhpCourseRule
     {
+        public List<BotRule> PHPCourseRule = new List<BotRule>()
+        {
+            new BotRule(
+                Name: "setcoursename",
+                Weight: 10,
+                MessagePattern: new Regex("(course name is|course is) (now )?(.*)", RegexOptions.IgnoreCase),
+                Process: delegate(Match match, ChatSessionInterface session) {
+                    session.SessionStorage.Values["CourseName"] = match.Groups[3].Value;
+                    return "Course name now is " + session.SessionStorage.Values["CourseName"];
+                }
+            ),
+
+            new BotRule(
+                Name: "getcoursename",
+                Weight: 10,
+                MessagePattern: new Regex("(what course name|(what is|say) course name)", RegexOptions.IgnoreCase),
+                Process: delegate(Match match, ChatSessionInterface session) {
+                    if (!session.SessionStorage.Values.ContainsKey("CourseName"))
+                    {
+                        return "I do not know course name";
+                    }
+                    if (match.Value.ToLower() == "what course")
+                    {
+                        return "course is " + session.SessionStorage.Values["CourseName"];
+                    }
+                    return "Youre in " + session.SessionStorage.Values["CourseName"];
+                }
+            ),
+        };
     }
 }
---
+++ Project_OLP_Rest.Test/Tests/PHPCourseRegexTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QXS.ChatBot;
+using QXS.ChatBot.ChatSessions;
+using QXS.ChatBot.Rules;
+
+namespace Project_OLP_Rest.Test.Tests
+{
+    [TestClass]
+    public class PHPCourseRegexTest
+    {
+        private RestChatBot PHPChatBot;
+
+        PhpCourseRule phpCourseRule = new PhpCourseRule();
+
+        [TestMethod]
+        public void CreatePHPBot() => PHPChatBot = new RestChatBot(phpCourseRule.PHPCourseRules);
+
+        [TestMethod]
+        public void PHPCourseNameTest()
+        {
+            CreatePHPBot();
+            string Message = "What course name";
+
+            ChatSessionInterface session = new RestChatSession();
+            Assert.AreEqual(PHPChatBot.FindAnswer(session, Message), "I do not know course name");
+
+            Console.WriteLine(PHPChatBot.FindAnswer(session, Message));
+
+        }
+    }
+}
