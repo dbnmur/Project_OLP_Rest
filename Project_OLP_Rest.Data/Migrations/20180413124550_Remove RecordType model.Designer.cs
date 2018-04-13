6718ff0afa88cf3733a9ef4e713f213bcb57947f
dbnmur <nerijus.muranovas@gmail.com>
2018-04-13 16:15:41 +0300

    Remove RecordType model

--- Project_OLP_Rest.Data/Migrations/20180413124550_Remove RecordType model.Designer.cs
+++ Project_OLP_Rest.Data/Migrations/20180413124550_Remove RecordType model.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Project_OLP_Rest.Data.Migrations
 {
     [DbContext(typeof(OLP_Context))]
-    partial class OLP_ContextModelSnapshot : ModelSnapshot
+    [Migration("20180413124550_Remove RecordType model")]
+    partial class RemoveRecordTypemodel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
