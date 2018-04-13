bf8b7c47e497e7f2ff2178b2584150b405ebd943
dbnmur <nerijus.muranovas@gmail.com>
2018-04-13 06:57:41 +0300

    Update ChatBot Model

--- Project_OLP_Rest.Domain/ChatBot.cs
+++ Project_OLP_Rest.Domain/ChatBot.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Project_OLP_Rest.Domain
 {
     public class ChatBot : Entity
     {
         public int ChatBotId { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 255, MinimumLength = 6)]
         public string Name { get; set; }
 
         public List<ChatSession> ChatSessions { get; set; }
