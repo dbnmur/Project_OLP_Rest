33cd602ca14c6f1538bab3b528c0c8e61e1dd3db
Lukas <MiniBig@users.noreply.github.com>
2018-04-19 19:10:31 +0300

    Add get courses steps

---
+++ Test.Cucumber/Steps/GetCoursesSteps.cs
@@ -0,0 +1,58 @@
+﻿using NFluent;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using TechTalk.SpecFlow;
+
+namespace Test.Cucumber
+{
+    [Binding]
+    public class GetCoursesSteps
+    {
+        private IWebDriver webDriver;
+
+        [BeforeScenario]
+        public void InitScenario()
+        {
+            webDriver = new FirefoxDriver();
+        }
+
+        [AfterScenario]
+        public void TearDownScenario()
+        {
+            webDriver.Dispose();
+        }
+
+        [Given(@"Im on home page")]
+        public void GivenImOnHomePage()
+        {
+            webDriver.Navigate().GoToUrl("http://localhost:3000/");
+        }
+        
+        [When(@"I have pressed login button")]
+        public void WhenIHavePressedLoginButton()
+        {
+            webDriver.FindElement(By.ClassName("loginForm")).FindElement(By.TagName("a")).Click();
+        }
+        
+        [Then(@"The result should display all courses")]
+        public void ThenTheResultShouldDisplayAllCourses()
+        {
+            var title = webDriver.Url;
+            Check.That(title).Equals("http://localhost:3000/dashboard");
+        }
+        
+        [Then(@"I press desired course")]
+        public void ThenIPressDesiredCourse()
+        {
+            webDriver.FindElement(By.ClassName("MuiGrid-typeItem-118")).FindElement(By.TagName("a")).Click();
+        }
+        
+        [Then(@"I get a course page")]
+        public void ThenIGetACoursePage()
+        {
+            var title = webDriver.Url;
+            Check.That(title).Equals("http://localhost:3000/courses/1");
+        }
+    }
+}
---
+++ Test.Cucumber/Tests/GetCourses.feature
@@ -0,0 +1,12 @@
+﻿Feature: GetCourses
+	In order to get course
+	As a student or a teacher
+	I want to see course
+
+@mytag
+Scenario: Get course
+	Given Im on home page
+	When I have pressed login button
+	Then The result should display all courses
+	Then I press desired course
+	Then I get a course page
